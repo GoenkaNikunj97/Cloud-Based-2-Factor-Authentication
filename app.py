f89fb67de12e8f53ebaf2b9d2f1133bb5920695c
NikunjGoenka <Nikunj.Goenka#dal.ca>
2021-03-09 19:16:07 +0530

    loginpage

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import *
 
+SAVED_USERNAME = "admin"
+SAVED_PASSWORD = "admin"
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def showLoginPage():
+   return render_template("index.html")
 
+@app.route('/login',methods = ['POST'])
+def login():
+   if request.method == 'POST':
+
+      print(request.form['userName'])
+      user = request.form['userName']
+      password = request.form['password']
+      #print(user + "  -->  "+ password)
+      if (user == SAVED_USERNAME and password == SAVED_PASSWORD):
+         return redirect(url_for('success',name = user))
+      else:
+         return render_template("index.html", message = "INVALID USERNAME OR PASSWORD")
+   else:
+      print("not POST METHOD")
+
+@app.route('/success/<name>')
+def success(name):
+   return render_template('welcome.html', name1 = name)
 
 if __name__ == '__main__':
     app.run()
