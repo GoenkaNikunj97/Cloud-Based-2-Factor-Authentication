2bef1dfa741159ff739065e1ad31f3b17ee51d87
NikunjGoenka <Nikunj.Goenka#dal.ca>
2021-04-04 22:51:38 -0300

    encryption

---
+++ cryptoLogic.py
@@ -0,0 +1,40 @@
+'''
+https://nitratine.net/blog/post/encryption-and-decryption-in-python/
+'''
+import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet
+
+def encrypt(message, password):
+    password = password.encode()
+    message = message.encode()
+    salt = b'salt_'
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    f = Fernet(key)
+    encryptedMessage = f.encrypt(message)
+    return encryptedMessage
+
+def decrypt(encryptedMessage, password):
+    password = password.encode()
+    encryptedMessage = encryptedMessage.encode()
+    salt = b'salt_'
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    f = Fernet(key)
+    message = f.decrypt(encryptedMessage)
+    return message.decode()
--- loginLogic.py
+++ loginLogic.py
@@ -1,14 +1,21 @@
 import wget
 import os
 from os import path
+import requests
 
+import cryptoLogic
+
+BASE_URL = "https://7mqeagvdnf.execute-api.us-east-1.amazonaws.com/"
+success = "Success"
 SAVED_USERNAME = "admin"
 SAVED_PASSWORD = "admin"
 
 def getSeedData():
     if(path.exists("C:\\loanApplication\\seed.txt")):
         temp = open("C:\\loanApplication\\seed.txt", "r")
-        return temp
+        data = temp.read()
+        temp.close()
+        return data
     return False
 
 def isUserValid(userId, password):
@@ -16,24 +23,38 @@ def isUserValid(userId, password):
 
     if(seed):
         print("Local Seed Found");
-        #print(seed.read())
-        # Check if User and password is Correct
-        if (userId == SAVED_USERNAME and password == SAVED_PASSWORD):
+
+        decryptedSeed = cryptoLogic.decrypt(seed , password)
+
+        decryptedSeed = hash(decryptedSeed)
+
+        queryUrl = BASE_URL + "validate_hash?hash=" + str(decryptedSeed)
+        res = requests.get(queryUrl)
+
+        if (res.status_code == 200):
             return True
         else:
             return False
     else:
         print("Seed Not Found - Checking UserID")
         # Check if UserId is there in DB
-        if(userId == SAVED_USERNAME):
+        queryUrl = BASE_URL+"validate_user?emailId="+userId
+        res = requests.get(queryUrl)
+        output = res.status_code
+        if(output == 200):
             print("UserID found Getting Seed")
-            url = "https://www.python.org/static/img/python-logo@2x.png"
+
+            queryUrl = "https://g52kmvfp98.execute-api.us-east-1.amazonaws.com/download_seed?emailId"+userId
+            res = requests.get(queryUrl)
+            seed = res.text
 
             is_accessible = os.access("C:\\loanApplication", os.F_OK)  # Check if you have access, this should be a path
             if is_accessible == False:  # If you don't, create the path
                 os.makedirs("C:\\loanApplication")
             os.chdir("C:\\loanApplication")  # Check now if the path exist
 
-            wget.download(url, 'C:\\loanApplication\\seed.png')
-
+            f = open("C:\\loanApplication\\seed.txt", "w")
+            encrptedSeed = cryptoLogic.encrypt(seed , password)
+            f.write(encrptedSeed.decode())
+            f.close()
             return True
